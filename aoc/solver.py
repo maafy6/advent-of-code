06b0fc28198216ece6a447ccc0c2bb5a9203d6ca
Matt Austin <Matt.Austin@exptechinc.com>
2023-12-06 11:27:02 -0500

    Improved solution output.

--- aoc/solver.py
+++ aoc/solver.py
@@ -1,8 +1,9 @@
 """Solving utilities."""
 import asyncio
 import inspect
-from typing import List, Optional
 from importlib import import_module
+from timeit import timeit
+from typing import Any, Awaitable, Callable, List, Optional, Union
 
 import aocd
 import pytest
@@ -25,19 +26,29 @@ def solve(year: int, day: int, parts: List[int], submit: bool = False) -> int:
         return 1
 
     rc = 0
+    solutions = {}
+    print(f"{year} - {day:02d}\n=========")
     for part in sorted(parts):
         if hasattr(module, f"part{part}"):
             func = getattr(module, f"part{part}")
-            solution = func()
-            if inspect.isawaitable(solution):
-                loop = asyncio.get_event_loop()
-                solution = loop.run_until_complete(solution)
 
-            print(f"Part {part}: {solution}")
+            def soln_func(
+                func: Callable[[], Union[Any, Awaitable[Any]]] = func, part: int = part
+            ) -> None:
+                if inspect.iscoroutinefunction(func):
+                    loop = asyncio.get_event_loop()
+                    solution = loop.run_until_complete(func())
+                else:
+                    solution = func()
+
+                solutions[part] = solution
+
+            soln_time = timeit(soln_func, number=1)
+            print(f"Part {part}: {solutions[part]} ({soln_time:.03f} s)")
 
             if submit:
                 part_id = {1: "a", 2: "b"}
-                aocd.submit(solution, year=year, day=day, part=part_id[part])
+                aocd.submit(solutions[part], year=year, day=day, part=part_id[part])
         else:
             print(f"No solution for AOC {year}-{day:02d} Part {part}")
             rc = 1
